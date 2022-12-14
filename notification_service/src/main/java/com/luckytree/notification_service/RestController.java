032063329bb0968bb923ddeedbff6a7dc52119f6
gowjr207@gmail.com <gowjr207@gmail.com>
2022-12-15 00:17:58 +0900

    [init] notification service

---
+++ notification_service/src/main/java/com/luckytree/notification_service/RestController.java
@@ -0,0 +1,23 @@
+package com.luckytree.notification_service;
+
+import com.luckytree.notification_service.config.FeignClients;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 통신 테스트를 위한 임시 컨트롤러 - 삭제 예정
+ */
+
+@RequiredArgsConstructor
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    private final FeignClients feignClients;
+
+    @GetMapping("/noti-test")
+    String startCall() {
+        String name = "junyeong";
+        String result = feignClients.call(name);
+        return result;
+    }
+}
